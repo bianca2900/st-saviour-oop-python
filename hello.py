4b7808461a0f09c0783cbb8b4b03c8a59b47dbf5
bmastroviti <bmastroviti@stsaviour.org>
2026-01-09 14:19:26 -0500

    lab 4

--- hello.py
+++ hello.py
@@ -1,11 +1,21 @@
 from movie import Movie
 from zombie_horror import ZombieHorror
+from romantic_comedy import RomanticComedy
 
 if __name__ == '__main__':
     # print('new dawn, new day')
-    twnety_eight_days_later = ZombieHorror('28 Days Later', 2.5, 28)
-    print(twnety_eight_days_later.create_suspense())
+    twenty_eight_days_later = ZombieHorror('28 Days Later', 2.5, 28)
+    print(twenty_eight_days_later.create_suspense())
 
-    assert isinstance(twnety_eight_days_later, Movie)
+    assert isinstance(twenty_eight_days_later, Movie)
 
-    print(str(twnety_eight_days_later))
+    print(str(twenty_eight_days_later))
+
+    how_to_lose_a_guy_in_ten_days= RomanticComedy('How To Lose A Guy in 10 Days', 2.5, 28)
+    print(twenty_eight_days_later.create_suspense())
+
+    assert isinstance(twenty_eight_days_later, Movie)
+
+    print(str(twenty_eight_days_later))
+
+    print(how_to_lose_a_guy_in_ten_days.laugh())
--- romantic_comedy.py
+++ romantic_comedy.py
@@ -1,17 +1,19 @@
-from romantic_comedy import Comedy
-def __init__(self, title: str, duration: float, laughs: str):
+from comedy import Comedy
+
+class RomanticComedy(Comedy):
+    def __init__(self, title: str, duration: float, laughs: str):
         super().__init__(title, duration, laughs)
         self.laughs = laughs
 
-        def self_laugh(self) -> str
-            laughs -= 1
-            if laughs > 0:
-                if laughs >= 10 and laughs < 20:
-                    return 'The movie ' + self.title + ' is making me loneley'
-                if laughs >= 20 and laughs < 30:
-                    return 'The movie ' + self.title +  'is calling me loneley in every way possible'
-                if laughs > 30:
-                    return  self.title + 'is so cringy' 
+    def laugh(self) -> str:
+        self.laughs -= 1
+        if self.laughs > 0:
+            if self.laughs >= 10 and self.laughs < 20:
+                return 'The movie ' + self.title + ' is making me lonely'
+            if self.laughs >= 20 and self.laughs < 30:
+                return 'The movie ' + self.title +  ' is calling me lonely in every way possible'
+            if self.laughs > 30:
+                return  self.title + ' is so cringy' 
                 
-            def __str__(self) -> str:
-             return f'title: {self.title}, duration: {self.duration}, laughs: {self.laughs}'
+    def __str__(self) -> str:
+        return f'title: {self.title}, duration: {self.duration}, laughs: {self.laughs}'
